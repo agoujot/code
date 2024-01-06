195cc5b45d35d865ef0d8bf85596b9dcd1968b2b
agoujot <145840578+agoujot@users.noreply.github.com>
2024-01-06 15:30:40 +0100

    added comments

--- CAT.ml
+++ CAT.ml
@@ -1,6 +1,6 @@
 open Graphics
-let ini () = Random.self_init(); open_graph ""; resize_window 1000 1020; set_color black; fill_rect 0 0 1000 1020
-let rec write l x y = 
+let ini () = Random.self_init(); open_graph ""; resize_window 1000 1020; set_color black; fill_rect 0 0 1000 1020 (* initializes the graphics screen & the RNG *)
+let rec write l x y = (* to write a list of strings one line each with the top one starting at x y *)
 	match l with
 	| [] -> ()
 	| h::t -> moveto x y; set_color white; draw_string h; write t x (y-20)
@@ -23,8 +23,8 @@ let info() = set_color black; fill_rect 0 0 1000 1020; write [
 " - d (Delete) : sets all the grid to black (it is therefore recommended that you have the state 'no life' and that it be black).";
 " - i (Info) : show this.";
 " - x (EXit) : closes CAT.";
-"Click to exit this and go back to pause."] 10 980; let rec wait () = if button_down() then () else wait() in wait()
-let rec b x e si =
+"Click to exit this and go back to pause."] 10 980; let rec wait () = if button_down() then () else wait() in wait() (* display info about this *)
+let rec b x e si = (* building an array array of size si filled with e() (to allow for different random results). should be called with x=0*)
 	let rec l y =
 		if y = si
 		then [||]
@@ -32,39 +32,39 @@ let rec b x e si =
     in if x = si
 	then [||]
 	else Array.append [|l 0|] (b (x+1) e si)
-let rec dcopy a i = if i = Array.length a then [||] else Array.append [|Array.copy a.(i)|] (dcopy a (i+1))
-let rec di g p f n si col d =
-	let g_ = dcopy g 0 in
-	let z = 1000/si in
-	let rec w k = if k >= si then k - si else if k < 0 then si + k else k in
-	let draw x y c = set_color c; fill_rect (x*z) (y*z) (z-1) (z-1) in
-	let bl s = set_color black; fill_rect 0 1000 1000 20; write [s] 10 1005 in
-	let rec cha i j e =
+let rec dcopy a i = if i = Array.length a then [||] else Array.append [|Array.copy a.(i)|] (dcopy a (i+1)) (* deepcopy of an array array, called with i=0 *)
+let rec di g p f n si col d = (* di for do it, main function that does pretty much everything. Arguments are: g for grid, an arrray array of cells (represented with Graphic.color s), p for parameter, a character defining the beheviou on that run, for f, n, si, and d just look up in info, and col is made out of the array of colors, its a function that just goes around the array *)
+	let g_ = dcopy g 0 in (* new array in which modifications will be made *)
+	let z = 1000/si in (* the widh of a cell in pixels *)
+	let rec w k = if k >= si then k - si else if k < 0 then si + k else k in (* w for wrap around *)
+	let draw x y c = set_color c; fill_rect (x*z) (y*z) (z-1) (z-1) in (* draws a cell, aka a rectangle. c is the color *)
+	let bl s = set_color black; fill_rect 0 1000 1000 20; write [s] 10 1005 in (* for bottom left even though now it is in the top left, eh, erases the text at the top and replaces it, for status *)
+	let rec cha i j e = (* cha for change, when you want to replace every cell with e(). *)
 		if i < 100
 		then (
 			if j = 100
 			then cha (i+1) 0 e
 			else let v = e() in g_.(i).(j) <- v; draw i j v; cha i (j+1) e) in
-	let rec it i j =
+	let rec it i j = (* it for iterate, as it does the main loop *)
 		if i = si
 		then ()
 		else
 			if j = si
 			then it (i+1) 0
 			else 
-				let rec ch l =
+				let rec ch l = (* ch for check, no ideas, applies the relatives coordinates in n to the coordinates of the current cell to get the coordinates of the cells to check.*)
 					match l with
 					| [] -> []
 					| h::t -> g.(w (i+fst(h))).(w (j+snd(h))):: ch t in
-				((if p = 's' then draw i j (g_.(i).(j)) else (g_.(i).(j) <- f (ch n) (g.(i).(j)); (if g.(i).(j) <> g_.(i).(j) then draw i j (g_.(i).(j))))); it i (j+1)) in
-	it 0 0; if not (button_down()) && p = 'p' then di g_ p f n si col d else
-	let rec wa() =
+				((if p = 's' (* for stop *) then draw i j (g_.(i).(j)) (* you only show *) else (g_.(i).(j) <- f (ch n) (g.(i).(j)); (if g.(i).(j) <> g_.(i).(j) then draw i j (g_.(i).(j))))); it i (j+1)) in
+	it 0 0; if not (button_down()) && p = 'p' (* for play*) then di g_ p f n si col d (*the standard path of just continuing*) else
+	let rec wa() = (* for wait *)
 		bl " - pause - ";
 		match read_key() with
 		| 'p' -> bl " - running - "; di g_ 'p' f n si col d
-		| 'o' -> di g_ 'o' f n si col d
+		| 'o' -> di g_ 'o' f n si col d (* meaning you only do it once *)
 		| 'e' -> bl " - editing - ";
-			let rec ed() = 
+			let rec ed() = (* for editing *)
 			if key_pressed() 
 			then 
 				(if read_key() = 'o' 
@@ -72,12 +72,12 @@ let rec di g p f n si col d =
 				else ed()) 
 			else 
 				(if (button_down())
-				then (let mx = fst(mouse_pos())/z and my = snd(mouse_pos())/z in g_.(mx).(my) <- col (g_.(mx).(my)); draw mx my (g_.(mx).(my)); Unix.sleepf 0.2; ed())
+				then (let mx = fst(mouse_pos())/z and my = snd(mouse_pos())/z (* coordinates of the cell you clicked*) in g_.(mx).(my) <- col (g_.(mx).(my)); draw mx my (g_.(mx).(my)); Unix.sleepf 0.2 (* else you would always change it multiple times*); ed())
 				else ed()) in ed()
 		| 'x' -> close_graph(); print_endline "Closed with X."
 		| 'd' -> cha 0 0 (fun () -> black); wa()
 		| 'n' -> cha 0 0 d; wa()
-		| 'i' -> info(); let rec show i j = if i = si then wa() else if j = si then show (i+1) 0 else (draw i j (g_.(i).(j)); show i (j+1)) in show 0 0
+		| 'i' -> info(); let rec show i j = if i = si then wa() else if j = si then show (i+1) 0 else (draw i j (g_.(i).(j)); show i (j+1)) (*need to redisplay after showing text*) in show 0 0
 		| _ -> print_endline "Unknown command. Press i for info."; wa()
 	in wa()
-let go f n si co d = info(); di (b 0 d si) 's' f n si (fun c -> let rec it i = if co.(i) = c then i else it (i+1) in co.(((it 0) + 1) mod (Array.length co))) d
+let go f n si co d = info(); di (b 0 d si) 's' (* for stop, doesnt even do first iteration*) f n si (fun c -> let rec it i = if co.(i) = c then i else it (i+1) in co.(((it 0) + 1) mod (Array.length co))) (* makes the fucntion out of the array *) d
