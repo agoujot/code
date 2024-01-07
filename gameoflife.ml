c49133938e03c67b4cc77d77777f986467b48fa8
agoujot <145840578+agoujot@users.noreply.github.com>
2024-01-07 17:49:35 +0100

    Delete gameoflife.ml
    see beam.ml

--- gameoflife.ml
+++
@@ -1,8 +0,0 @@
-open Graphics
-open CAT
-let () = ini(); go
-(fun l v -> let rec cou l = match l with | [] -> 0 | i::s -> if i = white then 1+cou s else cou s in let c = cou l in if c = 3 then white else if c = 2 then v else black)
-[(1, 1);(1, 0);(1, -1);(0, 1);(0, -1);(-1, 1);(-1, 0);(-1, -1)]
-100
-[|white; black|]
-(fun () -> let r = Random.int 2 in if r = 1 then white else black)
