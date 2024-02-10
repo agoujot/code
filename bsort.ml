23ef4fc059dfa95c59e84fa94bd9149c7d93d77c
agoujot <145840578+agoujot@users.noreply.github.com>
2024-02-10 09:22:39 +0100

    Delete bsort.ml
    going to put them all in one file

--- bsort.ml
+++
@@ -1,7 +0,0 @@
-open Graphics
-open Sortlib
-let rec bsort i j =
-	if j = 0 then () else
-	if i = j then (bsort 0 (j-1)) else
-	(if t.(i) > t.(i+1) then swap i (i+1); bsort (i+1) j)
-let () = set_window_title "Bubble sort"; bsort 0 999; off()
