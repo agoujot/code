459bf82b93a47bac7335b5bf9dbebab7f3a8b57e
agoujot <145840578+agoujot@users.noreply.github.com>
2024-02-10 09:24:15 +0100

    Delete isort.ml
    see bsort

--- isort.ml
+++
@@ -1,8 +0,0 @@
-open Graphics
-open Sortlib
-let rec isort i =
-	if i = 1000 then () else
-	let rec it j =
-		if (pause 10000; j > 0 && t.(j-1) > t.(j)) then (swap j (j-1); it (j-1)) else () in
-	it i; isort (i+1)
-let () = set_window_title "Insertion sort"; isort 0; off()
