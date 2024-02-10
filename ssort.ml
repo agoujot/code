01a56094253366e8f1b11303c2b729f74e8a76b0
agoujot <145840578+agoujot@users.noreply.github.com>
2024-02-10 09:25:53 +0100

    Delete ssort.ml
    see bsort

--- ssort.ml
+++
@@ -1,9 +0,0 @@
-open Graphics
-open Sortlib
-let rec fm i = if i = 999 then i else let ni = fm (i+1) in if (pause 10000;  t.(i) < t.(ni)) then i else ni
-let rec ssort di =
-	if di = 1000 then () else
-	let min_i = fm di in
-	swap di min_i;
-	ssort (di +1)
-let () = set_window_title "Selection sort"; ssort 0; off()
