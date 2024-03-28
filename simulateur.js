b1236525ccece74e715ad876d79fe6372c91e90e
agoujot <145840578+agoujot@users.noreply.github.com>
2024-03-28 20:10:25 +0100

    update simulateur
    changed load / save (not a fully working version, eidting is broken and temporarily disabled

--- simulateur.js
+++ simulateur.js
@@ -1,39 +1,24 @@
 // TODO:
-//		vars dans URL
+//		réparer la modification
 //		création de CA
 //		réoptimiser l'histoire
+//		compresser encore plus l'histoire (huffman ?)
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 kill = false; // si le CA devrait etre arrete
 CAlist = document.getElementById("CAlist");
-Infobox = document.getElementById("info");
-Options = document.getElementById("Options");
-Settings = document.getElementById("Settings");
-Download = document.getElementById("Download");
-pbtn = document.getElementById("p"); // p(ause)
-abtn = document.getElementById("a"); // a(vancer)
-plbtn = document.getElementById("+"); // pl(us)
-rbtn = document.getElementById("r"); // r(eculer)
-mobtn = document.getElementById("-"); // mo(ins)
-mbtn = document.getElementById("m"); // m(odifier)
-nbtn = document.getElementById("n"); // n(ouveau)
-sbtn = document.getElementById("s"); // s(auver)
-ebtn = document.getElementById("e"); // e(ffacer)
-dbtn = document.getElementById("d"); // d(ownload)
-ubtn = document.getElementById("u"); // u(pload)
-xbtn = document.getElementById("x");
-ibtn = document.getElementById("i");
-delay = document.getElementById("delay");
-size = document.getElementById("size");
-CA = document.getElementById("CA");
-wrap = document.getElementById("wrap");
+info = document.getElementById("info");
 fileurl = null;
 var whereami; // les indices dans groupes des groupes d'elements qu'on veut montrer
-groupes = [canvas, Options, CAlist, Infobox, Settings, Download, Upload];
+groupes = [canvas, Options, CAlist, info, Settings, Save, Upload];
 CAs = ["gol", "bosco", "daynight", "marine", "sparks", "eiii"];
-pauseonlybuttons = [abtn, plbtn, mobtn, rbtn, mbtn, nbtn, ebtn, dbtn, ubtn];
+pauseonlybuttons = [abtn, plbtn, mobtn, rbtn, mbtn, nbtn, ebtn, sbtn, cbtn];
 key_funcs = []; // cles : touches, valeurs : fonctions
 key_funcs[" ".charCodeAt(0)] = spacebar; // cas spécial, pas lié à un bouton.
+settingsvalid.onclick = () => {
+	if ( 1000 % size.value == 0 ) { only (whereami) } 
+	else { alert("Entree invalide. La taille doit etre un diviseur de 1000.") }
+}
 bind(() => restart(true, false), abtn, String.fromCharCode(39)); // fleche de droite
 bind(() => restart(true, true), plbtn, String.fromCharCode(38)); // d'en haut
 bind(() => restart(false, false), rbtn, String.fromCharCode(37)); // de gauche
@@ -42,47 +27,42 @@ bind(() => {
 	g_ = b(e(col), si); 
 	for (let i = 0; i < si; i++) {
 		for (let j = 0; j < si; j++) {
-			draw(i, j, g_[i][j])
+				draw(i, j, g_[i][j])
 		}
 	};
+	h = []
 	},
-	nbtn, "N");
-bind(() => {
-	hide([mbtn, abtn, plbtn, rbtn, mobtn, nbtn, ebtn]);
-	show_(sbtn);
-	canvas.onclick = function(e) {
-		if (sbtn.style.display != "none") {
-			rect = e.target.getBoundingClientRect();
-			mx = Math.floor((e.clientX - rect.left)/z);
-			my = Math.floor((e.clientY - rect.top)/z);
-			old_c = g_[mx][my];
-			g_[mx][my] = col[(fi(old_c)+1)%col.length];
-			draw(mx, my, g_[mx][my]) 
-		}
-	};
+	nbtn, "N"); // nouveau
+bind(() => { alert("Editing temporarily disabled.")
+//	hide(pauseonlybuttons);
+//	show_(vbtn);
+//	canvas.onclick = (e) => {
+//		if (vbtn.style.display != "none") {
+//			rect = e.target.getBoundingClientRect();
+//			mx = Math.floor((e.clientX - rect.left)/z);
+//			my = Math.floor((e.clientY - rect.top)/z);
+//			old_c = g[mx][my];
+//			g[mx][my] = col[(fi(old_c)+1)%col.length];
+//			draw(mx, my, g[mx][my]) 
+//		}
+//	};
 	}, 
-	mbtn, "M");
-bind(() => {
-	for (let i = 0; i < si; i++) {
-		for (let j = 0; j < si; j++) {
-			g_[i][j] = "0 0 0";
-		}
-	};
-	ctx.fillStyle = "rgb(0 0 0)";
-	ctx.fillRect(0, 0, 1000, 1000);
+	mbtn, "M"); // modifier
+bind(() => { alert("Editing temporarily disabled.")
+//	for (let i = 0; i < si; i++) {
+//		for (let j = 0; j < si; j++) {
+//				g_[i][j] = "0 0 0";
+//		}
+//	};
+//	ctx.fillStyle = "rgb(0 0 0)";
+//	ctx.fillRect(0, 0, 1000, 1000);
+//	h = [];
 	},
-	ebtn, "E")
-bind(() => { hide_(sbtn); show([mbtn, abtn, plbtn, rbtn, mobtn, nbtn, ebtn]) }, sbtn, "S");
-function spacebar () {
-	if (pbtn.style.display != "none") {
-		paused = true
-	} else {
-		restart(d, false)
-	}
-}
-bind((() => paused = true ), pbtn);
+	ebtn, "E") // effacer
+bind(() => { hide_(vbtn); show(pauseonlybuttons) }, vbtn, "V"); // valider (modifier)
+bind((() => paused = true ), pbtn); // pause
 bind((() => {
-	text = CA.value + "\n" + si.toString() + "\n" + col.join() + "\n" + delay.value.toString() + "\n" + wrap.checked + "\n" + compress(g_);
+	text = totext();
 	file = new Blob([text]);
 	if (fileurl) {
 		window.URL.revokeObjectURL(fileurl)
@@ -90,94 +70,198 @@ bind((() => {
 	fileurl = window.URL.createObjectURL(file);
 	Download.href = fileurl;
 	Download.download = "AC-"+ Math.floor(Date.now()/1000).toString() + ".txt";
-	only([Download]);
+	Download.innerHTML = "Télécharger "+ Download.download;
+	stateidout.innerHTML = totext();
+	only([Save]);
 	}), 
-	dbtn, "D")
-bind((() => {only([Upload])}), ubtn, "U")
-bind((() => { kill = true; choose() }), xbtn, "X");
-bind(info, ibtn, "I");
-hide([rbtn, mobtn, sbtn, dbtn, ubtn]);
+	sbtn, "S") // sauvegarder
+bind((() => {stateidout.style="background-color:white";  only([Upload])}), cbtn, "C") // charger
+bind((() => { kill = true; choose() }), xbtn, "X"); // fermer
+bind(() => only([info]), ibtn, "I"); // informations
+stateidout.onclick = () => { 
+	navigator.clipboard.writeText(totext());
+	stateidout.style = "background-color:darkgrey";
+}
+url = new URL (window.location.href);
+hide([rbtn, mobtn, vbtn]);
+save = (new URLSearchParams(url.search)).get("save");
 black = "0 0 0";
 red = "255 0 0";
 green = "0 255 0";
 blue = "0 0 255";
 white = "255 255 255"; // quelques couleurs predefinies
-choose();
+if (save) {
+	load(save)
+} else {
+	choose();
+}
 paused = true;
 filecontent = "";
-nameofcurrent = "";
-filename.addEventListener("change", fileListener);
+loadvalid.addEventListener("click", loadlistener);
 window.addEventListener("keydown", keyListener);
-CA.addEventListener("change", (() => {if ( CA.value != "none") { go(true) } } ));
-function fileListener (e) {
-	only(whereami);
-	files = e.target.files;
-	file = files[0];
-	reader = new FileReader();
-	reader.onload = ((a) => {filecontent = a.target.result})
-	reader.readAsText(file);
-	waitforfile()
+CAvalid.addEventListener("click", (() => {
+	switch (CA.value) { 
+		case "none" : 
+			break
+		case "import" : 
+			only([Upload]);
+			break
+		default : 
+			go(true)
+	} 
+}));
+function totext() { // renvoie une chaine decrivant etape actuelle, parametres et CA
+	return (
+	CA.value + "-" + 
+	si.toString() + "-" + 
+	col.map((s) => s.split(" ").join(".")).join() + "-" + 
+	delay.value.toString() + "-" + 
+	wrap.checked + "-" + 
+	compress(g_))
 }
-function waitforfile() {
+function spacebar () { // pour faire à la fois play et pause
+	if (pbtn.style.display != "none") {
+		paused = true
+	} else {
+		restart(d, false)
+	}
+}
+function loadlistener () {
+	if (stateidin.value == "") {
+		files = filename.files;
+		file = files[0];
+		reader = new FileReader();
+		reader.onload = ((a) => {filecontent = a.target.result})
+		reader.readAsText(file);
+		waitforfile()
+	} else {
+		load(stateidin.value)
+	}
+}
+function waitforfile() { // pour ne pas que load s'exécute avant que filecontent existe
 	if (filecontent == "") {
 		setTimeout(waitforfile, 100) 
 	} else {
-		load()
+		load(filecontent)
 	}
 }
-function load() {
-	lines = filecontent.split("\n");
+function load(s) {
+	lines = s.split("-");
 	kill = true;
 	CA.value = lines[0];
 	size.value = lines[1];
-	col = lines[2].split(",");
+	col = lines[2].split(",").map((s) => s.split(".").join(" "));
 	delay.value = Number(lines[3]);
 	wrap.checked = lines[4];
 	g = decompress(lines[5]);
 	go(false)
 }
-function compress (aa) {
+function tob62(n) { // en base 62, cad 0 .. 9 + A .. Z + a .. z. Assume, dans son implementation actuelle, que il y a moins de 62*62 couleurs.
+	res = ""
+	if (n == 0) {
+		return "0"
+	} else if (n < 10) {
+		return n.toString()
+	} else if (n < 36) {
+		return String.fromCharCode(n-10+65)
+	} else if (n < 62) {
+		return String.fromCharCode(n-36+97)
+	} else {
+		return ("'"+tob62(Math.floor(n / 62)) + tob62(n % 62))
+	}
+}
+function fromb62(s) {
+	for (let i = 0; i < s.length; i++) {
+		if (s >= "0" && s <= "9") {
+			return Number(s)
+		} else if (s >= "A" && s <= "Z") {
+			return (s.charCodeAt()-65+10)
+		} else {
+			return (s.charCodeAt()-97+36)
+		}
+	};
+}
+function compress (aa) { // tableau de tableau -> chaine
 	res = "";
 	coltoi = []; // pour ne pas refaire beaucoup de fois fi
 	for (let i = 0; i < col.length; i++) {
 		coltoi[col[i]] = i
 	};
+	lt = -1;
+	ti = 0;
 	for (let i = 0; i < si; i++) {
-		lt = -1;
-		ti = 0;
-		for (let j = 0; j < si; j++) {
-			k = coltoi[aa[i][j]];
-			if (k != lt) {
-				if (lt != -1) {
-					res += "," + lt.toString() + "," + ti.toString();
-				};
-				lt = k;
-				ti = 1;
+		if (i > 0 && JSON.stringify(aa[i]) == JSON.stringify(aa[i-1])) {
+			if (lt == "$") {
+				ti += 1;
 			} else {
-				ti += 1
+				lt = "$";
+				ti = 1
 			}
+		} else {
+			if (lt == "$") {
+				res += "$" + tob62(ti)
+			}
+			lt = -1;
+			ti = 0;
+			for (let j = 0; j < si; j++) {
+				k = coltoi[aa[i][j]];
+				if (k != lt) {
+					if (lt != -1) {
+						res += tob62(lt) + tob62(ti);
+					};
+					lt = k;
+					ti = 1;
+				} else {
+					ti += 1
+				}
+			}
+			res += tob62(lt) + tob62(ti);
 		}
-		res += "," + lt.toString() + "," + ti.toString();
-		res += "|"
 	};
 	return res
 }
-function decompress (s) {
-	res = s.split("|");
-	res = res.map((l) => {
-		l1 = l.split(",");
-		l1.shift();
-		l2 = [];
-		for (let i = 0; i < l1.length; i+=2) {
+function decompress (s) { // chaine -> tableau de tableau (a besoin des parametres)
+	res = [];
+	l = s.split("");
+	l1 = [];
+	i = 0;
+	while (i < l.length) {
+		if (l[i] == "'") {
+			l1.push(fromb62(l[i+1])*62+fromb62(l[i+2]));
+			i += 3
+		} if (l[i] == "$") {
+			l1.push("$");
+			l1.push(fromb62(l[i+1]));
+			i += 2
+		} else {
+			l1.push(fromb62(l[i]));
+			i ++
+		}
+	};
+	l2 = [];
+	c = 0;
+	i = 0;
+	while (i < l1.length) {
+		if (l1[i] == "$") {
 			for (let j = 0; j < l1[i+1]; j++) {
-				l2.push(col[l1[i]])
+				res.push(dcopy(res.slice(-1)[0]));
 			}
-		};
-		return l2 
-		});
+		} else {
+			for (let j = 0; j < l1[i+1]; j++) {
+				l2.push(col[l1[i]]);
+				c += 1;
+			};
+		}
+		i += 2;
+		if (c == Number(size.value)) {
+			c = 0;
+			res.push(dcopy(l2));
+			l2 = [];
+		}
+	}
 	return res
 }
-function moore(r) {
+function moore(r) { // voisinage de moore de rayon r
 	res = [];
 	for (let i = -r; i <= r; i++) {
 		for (let j = -r; j <= r; j++) {
@@ -195,7 +279,7 @@ function only(a) {
 	show(a);
 }
 function choose() { // efface canvas et passe au menu de selection
-	ctx.fillStyle = "rgb(0 0 0)";
+ctx.fillStyle = "rgb(0 0 0)";
 	ctx.fillRect(0, 0, 1000, 1000);
 	move([CAlist]);
 	CA.value = "none";
@@ -214,7 +298,7 @@ function pondered(caps) { // renvoie une fonction, qui prend un tableau de coule
 		}
 	}
 }
-function countof(x, a) {
+function countof(x, a) { // nombre de fois x dans a
 	count = 0; 
 	for (let i = 0; i < a.length; i++) {
 		if (a[i] == x) {
@@ -223,7 +307,7 @@ function countof(x, a) {
 	}
 	return count
 }
-function b_s (b, s) { // interprete la notation classique B/S, pour quand il y a deux types de vie.
+function b_s (b, s) { // interprete la notation classique B/S, pour quand il y a deux types de vie. voir rulestring sur lifewiki
 	b = b.split(",");
 	s = s.split(",");
 	b = b.map((e) => { if (e.indexOf("-") > -1) { return e.split("-").map(Number) } else { return Number(e) } } )
@@ -259,7 +343,7 @@ function b_s (b, s) { // interprete la notation classique B/S, pour quand il y a
 		}
 	} )
 }
-function b(e, si) { // construit un tableau de tableaux de si*si rempli avec les resultats de e()
+function b(e, si) { // b(uild), construit un tableau de tableaux de si*si rempli avec les resultats de e()
 	res = [];
 	for (let i = 0; i<si; i++) {
 		ligne = [];
@@ -299,16 +383,11 @@ function fi(co) { // Find Index, renvoie la position dans col où se trouve co
 			return i
 	}
 }
-function info() { if (!paused) { paused = true }; only([Infobox]) }
 function move(x) { whereami = x; only(x) }
-function validate() { // Pour les parametres
-	if ( 1000 % size.value == 0 ) { only (whereami) } 
-	else { alert("Entree invalide. La taille doit etre un diviseur de 1000.") }
-}
 function bind(f, b, c) { // attache au bouton b et a la touche c la fonction f
 	b.onclick = f;
 	if (c) {
-		key_funcs[c.charCodeAt(0)] = (() => {if (b.style.display != "none") {f.call()} })
+		key_funcs[c.charCodeAt(0)] = (() => {if (b.style.display != "none" && Options.style.display != "none") {f.call()} })
 	}
 };
 function keyListener(event) {
@@ -318,7 +397,7 @@ function keyListener(event) {
 	}
 };
 function restart(vd, vo) {
-	if (vd && !d) { // changement de sens
+	if (vd && !d) { // changement de sens d'arrière en avant nécessite de retourner
 		tmp = dcopy(g);
 		g = dcopy(g_);
 		g_ = tmp
@@ -334,7 +413,7 @@ function restart(vd, vo) {
 	};
 	di()
 }
-function di() { // Do It 
+function di() { // Do It, fait l'itération principale 
 	if (!kill) {
 		if (!paused) {
 			if (!d) {
@@ -352,44 +431,25 @@ function di() { // Do It
 					paused = true;
 				};
 			} else {
-		//		change = ""; // pour l'instant pas utilise
-		//		x = 0;
-		//		y = 0;
-		//		cc = 0; // derniere c(ouleur a)c(hanger)
 				for (let i = 0; i < si; i++) {
 					for (let j = 0; j < si; j++) { 
-						co = f(n.map((a) => { 
-						i_ = i+a[0];
-						j_ = j+a[1];
-						if (i_ < 0 || i_ >= si || j_ < 0 || j_ >= si) {
-							if (wrap.checked) {
-								return g[w(i_)][w(j_)]
+						co = f(n.map((a) => { // nouvelle couleur
+							i_ = i+a[0];
+							j_ = j+a[1];
+							if (i_ < 0 || i_ >= si || j_ < 0 || j_ >= si) {
+								if (wrap.checked) { // si il faut prendre de l'autre coté
+									return g[w(i_)][w(j_)]
+								} else { // si il faut compter une bordure de cases vides
+									return black
+								}
 							} else {
-								return black
+								return g[i_][j_]
 							}
-						} else {
-							return g[i_][j_]
-						}
-						}), g[i][j]); // nouvelle couleur
+							}), g[i][j]);
 						changed = (co != g[i][j]);
 						if (changed) {
 							g_[i][j] = co;
 							draw(i, j, co);
-			//				change = change.concat(",");
-			//				if (g[i][j] != cc) {
-			//					change = change.concat((fi(g[i][j])).toString())
-			//				};
-			//				change = change.concat(",");
-			//				if (j != y) {
-			//					change = change.concat((j-y).toString())
-			//				};
-			//				change = change.concat(",");
-			//				if (i != x+1) {
-			//					change = change.concat((i-y).toString())
-			//				};
-			//				x = i;
-			//				y = j;
-			//				cc = g[i][j];
 						}
 					}
 				};
@@ -398,9 +458,9 @@ function di() { // Do It
 			};
 			if (o) { 
 				o = false;
-			paused = true;
+				paused = true;
 			};
-			sleep(delay.value*1000).then(() => {di()}) // meme si delai nul, remedie a l'asynchronalite de javascript, cad l'empeche d'essayer de tout calculer avant d'afficher
+			sleep(delay.value*1000).then(() => {di()}) // meme si delai nul, remedie a l'asynchronalite de javascript, cad l'empeche d'essayer de tout calculer avant d'afficher, ce qui est problematique avec la boucle infinie intentionelle
 		} else {
 			hide_(pbtn);
 			show(pauseonlybuttons);
@@ -416,8 +476,8 @@ function inarray(l, co) {
 	}
 	return false;
 };
-function getpars() {
-	switch (CA.value) {
+function getpars(n) { // donne les parametres correspondant au nom n
+	switch (n) {
 		case "gol" :
 			return [
 			b_s("3", "2,3"),
@@ -467,7 +527,7 @@ function getpars() {
 			} ),
 			[[0, 1],[0,-1],[1,0],[-1,0],[-1,-1],[-1,1],[1,-1],[1,1]],
 			[black, red, green, blue],
-			pondered([0.5, 0.7, 0.9, 1])]
+			pondered([0.45, 0.65, 0.85, 1])]
 		case "eiii" :
 			return [
 				( (a, v) => {
@@ -484,14 +544,15 @@ function getpars() {
 				[black, red, green, blue],
 				equ]
 		case "rand" :
-			CA.value = CAs[randint(CAs.length)];
-			return getpars();
+			next =  CAs[randint(CAs.length)];
+			return getpars(next);
 		case "create":
-		case "import":
+		default: 
+			alert(n)
 	}
 }
-function go(rand) {
-	pars = getpars();
+function go(rand) { // lance le simulateur, avec une grille aléatoire si rand
+	pars = getpars(CA.value);
 	f = pars[0];
 	n = pars[1];
 	col = pars[2];
@@ -508,8 +569,8 @@ function go(rand) {
 		}
 	};
 	d = true; // d(irection), true pour avant et false pour arriere
-	o = false; // o(ne), si on ne fait qu'une generation
-	h = []; // h(istoire) des changements, pour aller en arriere
+	o = false; // o(nce), si on ne fait qu'une generation
+	h = []; // h(istoire) (pile en fait) des grilles, pour aller en arriere (optimisation supplementaire : faire l'histoire des changements a la place.)
 	g_ = dcopy(g); // grille d'apres
 	paused = true;
 	kill = false;
